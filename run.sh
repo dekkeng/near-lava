d686b6edd3a59734895b94107d985724cd9f097c
dekkeng <dek_keng@hotmail.com>
2024-03-01 17:26:15 +0700

    Readme

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ source ./config.txt
 
 i=0
 while true; do
-	echo "#$i Transfering $NEAR_LAVA_AMOUNT NEAR from $NEAR_LAVA_ACC1 to $NEAR_LAVA_ACC2"
+	echo "#$i Transfering $NEAR_LAVA_AMOUNT NEAR from ACC1 to ACC2"
 	near send-near $NEAR_LAVA_ACC1 $NEAR_LAVA_ACC2 $NEAR_LAVA_AMOUNT
-	echo "#$i Transfering $NEAR_LAVA_AMOUNT NEAR from $NEAR_LAVA_ACC2 to $NEAR_LAVA_ACC1"
+	echo "#$i Transfering $NEAR_LAVA_AMOUNT NEAR from ACC2 to ACC1"
 	near send-near $NEAR_LAVA_ACC2 $NEAR_LAVA_ACC1 $NEAR_LAVA_AMOUNT
 	i=$((i+1))
 done
