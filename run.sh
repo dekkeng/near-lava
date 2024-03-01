c0069eceebf3e7f1c768630ea4b7991efe3ee7a7
dekkeng <dek_keng@hotmail.com>
2024-03-01 17:37:40 +0700

    Fix

--- run.sh
+++ run.sh
@@ -7,5 +7,6 @@ while true; do
 	near send-near $NEAR_LAVA_ACC1 $NEAR_LAVA_ACC2 $NEAR_LAVA_AMOUNT
 	echo "#$i Transfering $NEAR_LAVA_AMOUNT NEAR from ACC2 to ACC1"
 	near send-near $NEAR_LAVA_ACC2 $NEAR_LAVA_ACC1 $NEAR_LAVA_AMOUNT
+	sleep 10
 	i=$((i+1))
 done
